f720d5be737afcdc528843eab10535d20b3ccd08
Matt Marini <mattmarini8765@gmail.com>
2024-09-16 17:56:13 +1000

    Winner logic finished

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ function GameBoard () {
         }
       }
 
-    const getBoard = () => console.log(board);
+    const getBoard = () => board;
 
     function move (piece, row, column) {
         board[row][column] = piece;
@@ -22,7 +22,7 @@ function GameBoard () {
 function Playgame () {
     const player1 = 'Player 1';
     const player2 = 'Player 2';
-    board = GameBoard();
+    const board = GameBoard();
     const players = [
         {
           name: player1,
@@ -42,17 +42,42 @@ function Playgame () {
     const getActivePlayer = () => activePlayer;
   
     const printNewRound = () => {
-      board.getBoard();
+      console.log(board.getBoard());
       console.log(`${getActivePlayer().name}'s turn.`);
     };
 
+    const winner = () => {
+        // ! This is all the possible winning combinations
+        const win_array = [[[0,0], [0,1], [0,2]], [[1,0], [1,1], [1,2]], [[2,0],[2,1],[2,2]], [[0,0],[1,1],[2,2]], [[2,0], [1,1], [0,2]], [[0,0], [1,0], [2,0]], [[0,1], [1,1], [2,1]], [[0,2], [1,2], [2,2]]];
+
+        // * Check status of board against all winning combos. 
+        // * If there are 3 x's or o's in the winning combo they get put in the array. If the array gets to 3 that player wins. 
+        for (let i = 0; i < win_array.length; i++) {
+            xs = [];
+            os = [];
+            for (let j = 0; j < 3; j++) {
+                x = win_array[i][j];
+                first = x[0];
+                second = x[1];
+                nitizen = board.getBoard();
+                if (nitizen[first][second] === 'X') {
+                    xs.push('X');
+                }
+                else if (nitizen[first][second] === 'O') {
+                    os.push('O');
+                }
+            if (xs.length === 3 || os.length === 3) {
+                console.log('Winner!');
+            }
+            }       
+        }
+    }
     const playRound = (row, column) => {
         // Drop a token for the current player
-        console.log(
-          `Horrible move ${getActivePlayer().name}`
-        );
+        console.log(`Horrible move ${getActivePlayer().name}`);
         board.move(getActivePlayer().token, row, column);
-    
+
+        winner();
         /*  This is where we would check for a winner and handle that logic,
             such as a win message. */
     
@@ -68,7 +93,8 @@ function Playgame () {
       // getActivePlayer for the UI version, so I'm revealing it now
       return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        winner
       };
 }
 
